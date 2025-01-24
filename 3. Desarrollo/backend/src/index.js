c7db7fe404e1cf9f37309bc33403b4fde60852fd
crisnk <cristobalalarcon22@gmail.com>
2025-01-24 15:40:03 -0300

    feat: desactiva el caché desde el backend (TEMPORAL)

--- 3. Desarrollo/backend/src/index.js
+++ 3. Desarrollo/backend/src/index.js
@@ -9,6 +9,22 @@ async function setupServer() {
     try {
         const app = express();
 
+        /* Desactivar caché para todas las respuestas
+        ¿Para qué?
+        Para que siempre se ejecute y/o se recargue la solicitud a la API, y siempre arroje solamente
+        un código HTTP 201, y no un 304 y luego un 201. Si se quita este bloque de código permitirá 
+        que el navegador utilice la caché de los datos guardados de la API cuando no hayan sido
+        modificados, esto es algo útil en cuanto a eficiencia para no tener que solicitar cada
+        vez que nada haya sido modificado. La idea de este bloque de código es que esté implementado
+        en fase de desarrollo y sea eliminado cuando salga a producción.
+        */
+        
+       // Desactivar caché para todas las respuestas
+        app.use((req, res, next) => {
+            res.setHeader('Cache-Control', 'no-store');
+            next();
+        });
+
         app.use(cors({ credentials: true, origin: true })); // Para permitir solicitudes al backend desde el frontend
         app.use(morgan('dev')); // Para mostrar por consola las peticiones
         app.use(json()); // Para aceptar el body en formato json de las solicitudes/peticiones
