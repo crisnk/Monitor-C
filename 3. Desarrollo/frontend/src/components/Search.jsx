7746bfb2a0a6f3368cb5accb515324035cb85042
crisnk <cristobalalarcon22@gmail.com>
2025-02-08 21:40:05 -0300

    fix: corrige la búsqueda mediante código de barras

--- 3. Desarrollo/frontend/src/components/Search.jsx
+++ 3. Desarrollo/frontend/src/components/Search.jsx
@@ -2,18 +2,30 @@ import { useState } from 'react';
 
 export default function SearchBar({ data, fields, onFilter }) {
     const [searchTerm, setSearchTerm] = useState('');
+    const [barcodeScanned, setBarcodeScanned] = useState(false);
 
     const handleSearchChange = (event) => {
-        const searchTerm = event.target.value;
-        setSearchTerm(searchTerm);
+        setSearchTerm(event.target.value);
 
-        const filteredData = data.filter(item => {
-            return fields.some(field =>
-                item[field].toString().toLowerCase().includes(searchTerm.toLowerCase())
-            );
-        });
+        const filteredData = data.filter(item =>
+            fields.some(field =>
+                item[field]?.toString().toLowerCase().includes(event.target.value.toLowerCase())
+            )
+        );
 
         onFilter(filteredData);
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault(); // Evita el envío de formulario si está dentro de uno
+            setBarcodeScanned(true); // Marca que el último input fue un escaneo
+        } else if (barcodeScanned) {
+            // Si se presiona cualquier tecla después del Enter
+            setSearchTerm(event.key); // Borra el input y escribe la nueva tecla
+            setBarcodeScanned(false); // Restablece el estado del escáner
+            event.preventDefault(); // Evita que la tecla se duplique
+        }
     };
 
     return (
@@ -24,6 +36,7 @@ export default function SearchBar({ data, fields, onFilter }) {
                 className="form-control"
                 value={searchTerm}
                 onChange={handleSearchChange}
+                onKeyDown={handleKeyDown} // Detecta Enter y otras teclas
             />
         </div>
     );
